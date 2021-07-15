bcc880784b671d12dcc20bb44550569a0276c693
GitHub Action <action@github.com>
2021-07-15 04:47:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/saihegde/sampleideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/saihegde/sampleideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.saihegde.sampleideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.saihegde.sampleideaplugin.MyBundle
 
 class MyApplicationService {
 
